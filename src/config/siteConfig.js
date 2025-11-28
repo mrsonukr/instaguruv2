d909dab14d57faaca9e71986585b7c4b2ba1a7b7
mrsonukr <MSSONUKR@GMAIL.COM>
2025-11-28 20:49:02 +0530

    h

--- src/config/siteConfig.js
+++ src/config/siteConfig.js
@@ -11,7 +11,7 @@ const siteConfig = {
   // Contact Information
   supportEmail: "help@smmguru.shop",
   supportPhone: "+91 72259 79671",
-  whatsappNumber: "917225979671",
+  whatsappNumber: "918210220189",
 
   // Payment Configuration - Multiple UPI IDs
   upiIds: ["netc.34161FA820328AA2D2560DE0@mairtel"],
